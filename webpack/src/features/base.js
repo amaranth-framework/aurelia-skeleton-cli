2bb39abcd25660effbb2b5ddd276ee34d8e4039c
Dragos Cirjan <dragos.cirja@itmediaconnect.ro>
2017-08-18 22:24:52 +0300

    see last commit

--- webpack/src/features/base.js
+++ webpack/src/features/base.js
@@ -1,4 +1,5 @@
-import { inject, LogManager } from 'aurelia-framework';
+// import { inject, LogManager } from 'aurelia-framework';
+import { inject } from 'aurelia-framework';
 import { AureliaConfiguration } from 'aurelia-configuration';
 import { I18N } from 'aurelia-i18n';
 import { Config as API } from 'aurelia-api';
@@ -6,12 +7,16 @@ import { EventAggregator } from 'aurelia-event-aggregator';
 import { Router } from 'aurelia-router';
 
 import UUID from 'uuid-js';
+import { excludes, traits } from 'traits-decorator'
 
+import {Loggable} from 'features/traits/loggable';
+import {RESTable} from 'features/traits/restable';
 import { waitForVariable, extend, className, parentClassName } from 'features/utils';
 
 /**
  * Aurelia class base for almost each functionality we may build.
  */
+@traits(Loggable::excludes('toString'),RESTable)
 @inject(API, AureliaConfiguration, EventAggregator, I18N, Router)
 export class Base {
     /**
@@ -23,7 +28,7 @@ export class Base {
      * @param {Router}               router Aurelia Router module
      */
     constructor(api, config, events, i18n, router) {
-        this.api = api;
+        this.configure(api);
         this.config = config;
         this.events = events;
         this.i18n = i18n;
@@ -35,31 +40,7 @@ export class Base {
         /**
          * @type {Logger}
          */
-        this.logger = LogManager.getLogger(this.toString());
-    }
-    /**
-     * Obtain REST Endpoint, functionig with 'aurelia-api' module.
-     * @return {Promise}
-     */
-    getRestEndpoint() {
-        return new Promise((resolve, reject) => {
-            if (this._rest) {
-                resolve(this._rest);
-                return;
-            }
-            // wait for config variable
-            waitForVariable(this.config)
-                // if timeout, throw error
-                .catch(err => {
-                    this.logger.error(err);
-                    reject(err);
-                })
-                // otherwise return REST endpoint
-                .then(config => {
-                    this._rest = this.api.getEndpoint(this.config.get('rest.http-endpoint'));
-                    resolve(this._rest);
-                })
-        });
+        this.logger = this.getLogger();
     }
     /**
      * Reduce class to a string identifier
