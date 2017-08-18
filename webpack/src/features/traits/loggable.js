7349a741371daaa1e8937facae6d9cdc26037ffc
Dragos Cirjan <dragos.cirja@itmediaconnect.ro>
2017-08-18 22:24:10 +0300

    implemented traits, and created Loggable trait

---
+++ webpack/src/features/traits/loggable.js
@@ -0,0 +1,27 @@
+import { LogManager } from 'aurelia-framework';
+
+import { className, parentClassName } from 'features/utils';
+
+/**
+ * Loggable class. Trait for class that needs logger object.
+ */
+export class Loggable {
+    /**
+     * Getter: Obtain logger instance for the class.
+     * @return {LogManager}
+     */
+    getLogger() {
+        if (!this.__logger) {
+            this.__logger = LogManager.getLogger(this.toString());
+        }
+        return this.__logger;
+    }
+
+    /**
+     * Reduce class to a string identifier
+     * @return {String}
+     */
+    toString() {
+        return `${parentClassName(this) || 'Object'}/${className(this)}}`
+    }
+}
