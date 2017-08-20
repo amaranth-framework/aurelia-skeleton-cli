38d792501295d681ced5adcc8f66040307b6e8c4
Dragos Cirjan <dragos.cirja@itmediaconnect.ro>
2017-08-21 00:44:20 +0300

    partial work with Forms ...

--- webpack/src/components/helper/form/form.js
+++ webpack/src/components/helper/form/form.js
@@ -1,7 +1,16 @@
+import {inject, NewInstance} from 'aurelia-framework';
+import {validateTrigger, ValidationController, ValidationRules} from 'aurelia-validation';
+
 import {Component} from 'features/views/component';
+import {extend} from 'features/utils';
 
-import Chance from 'chance';
-
+/**
+ * Component Helper for Forms
+ *
+ * For full aurelia-validation documentation, please read:
+ * http://aurelia.io/hub.html#/doc/article/aurelia/validation/latest/validation-basics/2
+ */
+@inject(NewInstance.of(ValidationController))
 export class ComponentHelperForm extends Component {
     /**
      * @see View:defaultSettings
@@ -16,59 +25,145 @@ export class ComponentHelperForm extends Component {
         content: {},  // translation keys for different text/html components in the template
 
         service: {},  // possible service settings for component
-        services: {}  // possible services settings for component
+        services: {}, // possible services settings for component
+
+        name: 'default'
     };
     /**
      * @see View::overrideSettingsKey
      */
     overrideSettingsKey = 'components.helper-table';
     /**
+     * @see Base::constructor()
+     * @see View::constructor()
+     * @param {any} validationController
+     */
+    constructor(validationController, ...args) {
+        super(...args);
+        /**
+         * @type ValidationController
+         */
+        this.vc = validationController;
+        this.vc.validateTrigger = validateTrigger.blur;
+    }
+    /**
+     * @see View::attached()
+     */
+    attached() {
+        this.events.publish(`form:${this.settings.name}:attached`, this);
+    }
+    /**
+     * Getter: Obtain form data object.
+     * @method data
+     * @return {Object}
+     */
+    get data() {
+        let chunks = this.inputs.map((input) => {
+            let chunk = {};
+            if (input.name) {
+                chunk[input.name] = input.value || null;
+            }
+            return chunk;
+        });
+        chunks.unshift({});
+        return extend.apply(null, chunks);
+    }
+    /**
+     * @see View::detached()
+     */
+    detached() {
+        super.detached();
+        this.events.publish(`form:${this.settings.name}:detached`, this);
+    }
+    /**
+     * Define the name for a certain input.
+     * @param  {Object} input
+     * @param  {Number} index
+     * @return {String}
+     */
+    getInputName(input, index = 0) {
+        let name = input.name ? input.name : 'input';
+        name = `${name}-${index}-${this.__uuid.toString()}`;
+        if (input.type === 'checkbox') {
+            name = `${name}[]`
+        }
+        return name;
+    }
+    /**
+     * @see View::init()
+     */
+    init() {
+        super.init();
+        this.eventsAdd(`form:${this.settings.name}:validate`, this.validate());
+        this.events.publish(`form:${this.settings.name}:init`, this);
+    }
+    /**
      * [isTextarea description]
-     * @param  {String}  type
+     * @param {Object}  input
      * @return {Boolean}
      */
     isCheckable(input) {
-        return /checkbox|radio/.test(input.value)
+        return /checkbox|radio/.test(input.type)
     }
     /**
-     * [isTextarea description]
-     * @param  {String}  type
+     * Determine whether the form has 'form-horizontal' style or not.
      * @return {Boolean}
      */
-    isCheckables(input) {
-        return /checkboxes|radios/.test(input.value)
+    isHorizontalForm() {
+        return this.style.indexOf('form-horizontal') > -1;
+    }
+    /**
+     * Determine whether the form has 'form--material' style or not.
+     * @return {Boolean}
+     */
+    isMaterialForm() {
+        return this.style.indexOf('form--material') > -1;
     }
     /**
      * [isTextarea description]
-     * @param  {String}  type
+     * @param {Object}  input
      * @return {Boolean}
      */
     isSelect(input) {
-        return input.value === 'select';
+        return input.type === 'select';
     }
     /**
      * [isTextarea description]
-     * @param  {String}  type
+     * @param {Object}  input
      * @return {Boolean}
      */
     isTextarea(input) {
-        return input.value === 'textarea';
+        return input.type === 'textarea';
     }
     /**
      * [isTextInput description]
-     * @param  {String}  type
+     * @param {Object}  input
      * @return {Boolean}
      */
-    isTextInput(type) {
-        return /color|date(time-local)?|email|image|month|number|password|range|search|te(l|xt)|time|url|week/.test(type)
+    isTextInput(input) {
+        return input.type === ''
+            || input.type === null
+            || input.type === undefined
+            || /^(color|date(time-local)?|email|image|month|number|password|range|search|te(l|xt)|time|url|week)$/.test(input.type);
     }
     /**
      * Calculate a marker for specifying the input is filled in, only for material style.
      * @method materialFilledMarker
-     * @param  {Object} input
+     * @param  {Object}  input
      * @return {String}
      */
     materialFilledMarker(input) {
-        return ((this.style.indexOf('form--material') > -1 && input.value) ? 'is-not-empty' : '');
+        return (this.isMaterialForm() && input.value) ? 'is-not-empty' : '';
+    }
+    /**
+     * Validate method.
+     */
+    async validate() {
+        const RESULT = await this.vc.validate();
+        if (RESULT.valid) {
+            this.events.publish(`form:${this.settings.name}:validated`, this.data);
+            return;
+        }
+        this.events.pubslih(`form:${this.settings.name}:invalid`);
     }
 }
--- webpack/src/templates/forms/forms.js
+++ webpack/src/templates/forms/forms.js
@@ -1,13 +1,10 @@
-/**
- * For full aurelia-validation documentation, please read: 
- * http://aurelia.io/hub.html#/doc/article/aurelia/validation/latest/validation-basics/2
- */
-import { inject, NewInstance } from 'aurelia-framework';
-import { validateTrigger, ValidationController, ValidationRules } from 'aurelia-validation';
+import { ValidationRules } from 'aurelia-validation';
+
+import messg from 'messg';
+import 'messg/index.css';
 
 import { Template } from 'features/views/template';
 
-@inject(NewInstance.of(ValidationController))
 export class TemplateForms extends Template {
     /**
      * @see View:defaultSettings
@@ -40,41 +37,130 @@ export class TemplateForms extends Template {
      */
     index = 1;
     /**
-     * @see Base::constructor()
-     * @see View::constructor()
-     * @param {any} validationController 
+     * Form settings.
+     * @type {Object}
      */
-    constructor(validationController, ...args) {
-        super(...args);
-        /**
-         * @type ValidationController
-         */
-        this.vc = validationController;
-        this.vc.validateTrigger = validateTrigger.blur;
+    inputs = {
+        basic: [
+            {
+                label: 'Regular Input',
+                type: 'text',
+                name: 'text',
+                helpblock: 'Simple helpblock for regular input'
+            },
+            {
+                label: 'Password',
+                type: 'password',
+            },
+            {
+                label: 'Email',
+                type: 'email',
+                name: 'email'
+            },
+            // {
+            //     label: 'File',
+            //     type: 'file',
+            // },
+            {
+                label: 'Textarea',
+                type: 'textarea',
+            },
+            {
+                label: 'Select',
+                type: 'select',
+                values: [
+                    { label: 'First Selectable Item', value: 0 },
+                    { label: 'Second Selectable Item', value: 1 },
+                    { label: 'Third Selectable Item', value: 2 },
+                    { label: 'Disabled Selectable Item', value: 3, disabled: true }
+                ]
+            },
+            {
+                label: 'Checkboxes',
+                type: 'checkbox',
+                values: [
+                    { label: 'First Selectable Item', value: 5 },
+                    { label: 'Second Selectable Item', value: 6 },
+                    { label: 'Third Selectable Item', value: 7 },
+                    { label: 'Disabled Selectable Item', value: 8, disabled: true }
+                ]
+            },
+            {
+                label: 'Radio Buttons',
+                type: 'radio',
+                values: [
+                    { label: 'First Selectable Item', value: 9 },
+                    { label: 'Second Selectable Item', value: 10 },
+                    { label: 'Third Selectable Item', value: 11 },
+                    { label: 'Disabled Selectable Item', value: 12, disabled: true }
+                ]
+            },
+            {
+                label: 'Checkboxes Inline',
+                type: 'checkbox',
+                style: 'form-group--inline',
+                values: [
+                    { label: 'First Selectable Item', value: 13 },
+                    { label: 'Second Selectable Item', value: 14 },
+                    { label: 'Third Selectable Item', value: 15 },
+                    { label: 'Disabled Selectable Item', value: 16, disabled: true }
+                ]
+            },
+            {
+                label: 'Radio Buttons Inline',
+                type: 'radio',
+                style: 'form-group--inline',
+                values: [
+                    { label: 'First Selectable Item', value: 17 },
+                    { label: 'Second Selectable Item', value: 18 },
+                    { label: 'Third Selectable Item', value: 19 },
+                    { label: 'Disabled Selectable Item', value: 20, disabled: true }
+                ]
+            },
+        ]
+    };
+    /**
+     * @see View::init()
+     */
+    init() {
+        super.init();
+        this.inputs.basic.forEach((input) => {
+            if (/checkbox|radio|select/.test(input.type)) {
+                input.value = input.values[1];
+                if (input.type === 'checkbox') {
+                    input.value = [input.value];
+                }
+            }
+        });
 
-        this.form = {};
+        this.eventsAdd('form:default:init', (form) => {
+            console.log(form, form.data);
+            ValidationRules
+                .ensure('text').displayName('Text Input')
+                    .required()
+                .ensure('email').displayName('Email Input')
+                    .email().withMessage(`\${$displayName} must be an email.`)
+                    .required()
+                .on(form.data);
+        });
+
+        this.eventsAdd('form:default:validated', (data) => {
+            messg.success('Form validated succesfully.');
+        });
+
+        this.eventsAdd('form:default:invalid', (data) => {
+            messg.success('Form invalid.');
+        });
     }
-    /**
-     * 
-     */
-    attached() {
-        ValidationRules
-            .ensure('input').displayName('Text Input')
-                .required()
-            .ensure('email').displayName('Email Input')
-                .email().withMessage(`\${$displayName} must be an email.`)
-                .required()
-            .on(this.form);
-    }
-    /**
-     * 
-     */
-    async submitSimpleForm() {
-        let result = await this.vc.validate();
-        if (result.valid) {
-            alert('Form is valid!');
-        } else {
-            this.logger.warn('Not validating!', this.vc.errors);
-        }
-    }
+    // /**
+    //  *
+    //  */
+    // async submitSimpleForm() {
+    //     let result = await this.vc.validate();
+    //     if (result.valid) {
+    //         alert('Form is valid!');
+    //     } else {
+    //         this.logger.warn('Not validating!', this.vc.errors);
+    //     }
+    // }
 }
